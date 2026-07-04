da67fe6dbe7d11700d7d53f5fb6eb3002dd22471
Andika Wahyudi <andika.aryowahyudi@gmail.com>
2026-07-04 14:25:56 +0800

    Initial Commit

---
+++ SAP2.2 AppScreens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SAP2.2 AppScreens
+//
+//  Created by Andika Wahyudi on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SAP2.2 AppScreens/SAP2_2_AppScreensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SAP2_2_AppScreensApp.swift
+//  SAP2.2 AppScreens
+//
+//  Created by Andika Wahyudi on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SAP2_2_AppScreensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
